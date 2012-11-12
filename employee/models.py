f46d8743d7c175c79bee1ab44e613647556f270f
Mafinar Khan <codeblade@gmail.com>
2012-11-12 14:13:33 +0600

    The reincarnation of README

--- employee/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from organization.models import *
-from base.models import BaseModel
-
-class Employee(BaseModel):
-    department = models.ForeignKey(Department)
-# Create your models here.
